05880d4229d2b167e676ef136d03e14cd64c93cf
testme2000 <nitin.dixfree@gmail.com>
2018-01-22 18:20:21 -0600

    Initial check-in

---
+++ Assignment/AngularService/BookLogger/public/app/services/currenuser.js
@@ -0,0 +1,13 @@
+'use strict';
+
+(function(){
+    angular.module('app').factory('currentUser', currentUser);
+    
+    function currentUser() {
+        return {
+            lastBookEdited : lastBookEdited
+        };
+        
+        var lastBookEdited = {};
+    }
+}());
