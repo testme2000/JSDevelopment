2be0a2ab5301b978f107d81abe9335a861fbcfbf
testme2000 <nitin.dixfree@gmail.com>
2018-02-25 16:53:26 -0600

    Initial check-in

---
+++ Assignment/AngularApp/SecondAssign/myapp.js
@@ -0,0 +1,8 @@
+'use strict';
+
+(function(){
+    
+    var myApp = angular.module("myApp",[]);
+    
+    
+}());
---
+++ Assignment/AngularApp/SecondAssign/mycontroller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+(function(){
+    var myApp = angular.module("myApp");
+    
+    myApp.controller("myController",function($scope) {
+        
+    });
+    
+    
+}());
