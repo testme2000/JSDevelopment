e21c55fb6157c7030cd2891b7b2b286fb4371a0e
Nitin Dixit <nitin.dixit@ncr.com>
2018-04-03 16:27:26 -0500

    Protractor implementation continue

--- Assignment/HeadFirst/MyBattleUsingAngular/Test/spec.js
+++ Assignment/HeadFirst/MyBattleUsingAngular/Test/spec.js
@@ -1,15 +1,30 @@
 describe('Battle Test using Protractor', function() {
     it('Load the board', function() {
-        browser.get('http://127.0.0.1:57207/Index.html');
+        browser.get('http://127.0.0.1:58000/Index.html');
         
         // Board should load successfully
         expect(browser.getTitle()).toEqual('Battleship Game (Using AngularJS)');
-        // Initial warning message should be on
-        var reqElement = element(by.id('reqId'));
-        console.log("NITIN SAY ");
+        // 1. Verify Initial error message
         element(by.id('reqId')).getText().then(function(text) {
             expect(text).toEqual('Guess input is mandatory');
         });
+        // 2. Verify error attribute is set properly
+        element(by.id('errorId')).getAttribute('ng-style').then(function(value) {
+            expect(value).toEqual('error_message');
+        });
+        // 3. Verify error_message has red color
+        element(by.id('errorId')).evaluate('error_message').then(function(value) {
+            expect(value['color']).toEqual('red');
+        });
+        // 3. Verify Guess input is empty
+        element(by.name('guessadd')).getText().then(function(text) {
+            expect(text.length).toEqual(0);
+        });
+        // 4. Verify Message Area is empty
+        element(by.id('messageArea')).getText().then(function(text) {
+            expect(text.length).toEqual(0);
+        });
+        var scop;
         console.log("IT COMPLETE NOW");
     });
 });
