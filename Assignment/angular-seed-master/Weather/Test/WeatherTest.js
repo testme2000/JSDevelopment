8b309875e50b3915f5e04ae632bf1964251c37ca
Nitin Dixit <nitin.dixit@ncr.com>
2018-04-21 11:11:07 -0500

    Mocha implementation started

--- Assignment/angular-seed-master/Weather/Test/WeatherTest.js
+++ Assignment/angular-seed-master/Weather/Test/WeatherTest.js
@@ -1,3 +1,31 @@
-var assert = require('assert');
-
-describe('')
+describe('weatherController', function() {
+    // Define variables for controller and service
+    var weatherController,weatherService;
+    var weatherConstant;
+    
+    
+    beforeEach(function() {
+        // Load the module, so we can start testing
+        module('weatherApp');
+        
+        // Get the weather Service
+        inject(function($controller,$http,_WEATHER_DETAIL_,_weatherService_) {
+            weatherService = _weatherService_;
+            
+            // Get Controller
+            weatherController = $controller('weatherController');
+            
+            // Get App constant
+            weatherConstant = _WEATHER_DETAIL_;
+        });
+    });
+    
+    describe('Constructor', function() {
+        it('Verify Weather Basic Setup', function() {
+            // Verify constant
+            assert.typeof(weatherConstant.url,'string');
+            console.log("TEst");
+        });
+    });
+    
+});
