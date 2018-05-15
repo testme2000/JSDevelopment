dee5f2dabfc119d8264b39fff41b3798a786a6e0
testme2000 <nitin.dixfree@gmail.com>
2018-05-14 20:58:25 -0500

    Timer added with webservice

--- Assignment/HTML5/Gumball/mighty.js
+++ Assignment/HTML5/Gumball/mighty.js
@@ -1,7 +1,7 @@
 'use strict';
 
 window.onload = function() {
-    
+    setInterval(pageRefresh,3000);
 }
 
 function updateSales(sales) {
@@ -14,4 +14,16 @@ function updateSales(sales) {
         div.innerHTML = sale.name + " sold " + sale.sales + " gumballs";
         salesDiv.appendChild(div);
     }
+}
+
+function pageRefresh() {
+    var url = "http://gumball.wickedlysmart.com?callback=updateSales";
+    
+    var newScriptElement = document.createElement("script");
+    
+    newScriptElement.setAttribute("src",url);
+    newScriptElement.setAttribute("id",jsonp);
+    
+    var oldScriptElement = document.getElementById("jsonp");
+    var head = document.getElement
 }
