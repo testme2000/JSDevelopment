c17a6efa0467f5e67dd383f644d8a65ca23c0be4
Nitin Dixit <nitin.dixit@ncr.com>
2018-05-25 15:30:30 -0500

    TShirt Design complete

--- Assignment/HTML5/T-Shirt/tweetshirt.js
+++ Assignment/HTML5/T-Shirt/tweetshirt.js
@@ -19,6 +19,13 @@ function previewHandler() {
             drawSquare(canvas,context);
         }
     }
+    else if(shape == "circles") {
+        for(var circles = 0;circles < 20;circles++) {
+            drawCircle(canvas,context);
+        }
+    }
+    drawText(canvas,context);
+    drawBird(canvas,context);
 }
 
 function drawSquare(canvas,context) {
@@ -43,4 +50,67 @@ function fillBackgroundColor(canvas, context) {
     context.fillStyle = bgColor;
     
     context.fillRect(0,0,canvas.width,canvas.height);
+}
+
+function drawCircle(canvas,context) {
+    var radius = Math.floor(Math.random() * 40);
+    var x = Math.floor(Math.random() * canvas.width);
+    var y = Math.floor(Math.random() * canvas.height);
+    
+    context.beginPath();
+    context.arc(x,y,radius,0,degreesToRadians(360), true);
+    context.fillStyle = "lightblue";
+    context.fill();
+}
+
+
+function degreesToRadians(degrees) {
+    return (degrees * Math.PI)/180;
+}
+
+
+function updateTweets(tweets) {
+    var tweetsSelection = document.getElementById("tweets");
+    
+    for(var i = 0;i < tweets.length;i++) {
+        var tweet = tweets[i];
+        
+        var option = document.createElement("option");
+        option.text = tweet.text;
+        option.value = tweet.text.replace("\"","'");
+        tweetsSelection.options.add(option);
+    }
+    tweetsSelection.selectedIndex = 0;
+}
+
+function drawText(canvas, context) {
+    var selectObj = document.getElementById("foregroundColor");
+    var index = selectObj.selectedIndex;
+    var fgColor = selectObj[index].value;
+    
+    context.fillStyle = fgColor;
+    context.font = "bold 1em sans-serif";
+    context.textAlign = "left";
+    context.fillText("I saw this tweet",20,40);
+    
+    selectObj = document.getElementById("tweets");
+    index = selectObj.selectedIndex;
+    var tweet = selectObj[index].value;
+    context.font = "italic 1.2em serif";
+    context.fillText(tweet, 30,100);
+    
+    
+    context.font = "bold 1em sans-serif";
+    context.textAlign = "left";
+    context.fillText("and all I got was this lousy t-shirt", canvas.width-280,canvas.height-40);
+}
+
+function drawBird(canvas,context) {
+    var twitterBird = new Image();
+    twitterBird.src ="twitterBird.png";
+    
+    twitterBird.onload = function() {
+        context.drawImage(twitterBird, 20,120,70,70);
+    }
+    
 }
