afeec4b86dcb986466a0474616ec0563eb8788e0
testme2000 <nitin.dixfree@gmail.com>
2018-06-01 20:13:33 -0500

    Video Playlist implementation

---
+++ Assignment/HTML5/VideoList/weblist.js
@@ -0,0 +1,45 @@
+"use strict";
+
+var position = 0;
+var playlist;
+var video;
+
+window.onload = function() {
+    playlist = [
+        "video/video/preroll",
+        "video/video/areyoupopular",
+        "video/video/destinationearth"
+    ];
+    
+    video = document.getElementById("video");
+    video.addEventListener("ended", nextVideo,false);
+    video.src = playlist[position] + getFormatExtension();
+    video.load();
+    video.play();
+    alert(video.src);
+}
+
+function nextVideo() {
+    position++;
+    
+    if(position >= playlist.length) {
+        position = 0;
+    }
+    
+    video.src = playlist[position] + getFormatExtension();
+    video.load();
+    video.play();
+    alert(video.src);
+}
+
+function getFormatExtension() {
+    if(video.canPlayType("video/mp4") != "") {
+        return ".mp4";
+    } else if(video.canPlayType("video/webm") != "") {
+        return ".webm";
+    } else if(video.canPlayType("video/ogg") != "") {
+        return ".ogv";
+    }
+    
+    
+}
