a6dbeb4cdbaed86a8029e5ec3704b6ccbd60fb00
testme2000 <nitin.dixfree@gmail.com>
2018-06-03 16:46:47 -0500

    Video booth updated with toggle button

--- Assignment/HTML5/VideoBooth/videobooth.js
+++ Assignment/HTML5/VideoBooth/videobooth.js
@@ -1,6 +1,14 @@
 'use strict';
 
+var videos = { video1: "video/demovideo1", video2: "video/demovideo2" };
+
 window.onload = function() {
+    
+    var video = document.getElementById("video");
+    video.src = videos.video1 + getFormatExtension();
+    video.load();
+    
+    
     var controlLinks = document.querySelectorAll("a.control");
     for(var iControl = 0;iControl < controlLinks.length;iControl++) {
         controlLinks[iControl].onClick = handleControl;
@@ -41,3 +49,67 @@ function handleControl(e) {
         }
     }
 }
+
+function setEffect(e) {
+    var id = e.target.getAttribute("id");
+    if (id == "normal") {
+        pushUnpushButtons("normal", ["western", "noir", "scifi"]);
+    } else if (id == "western") {
+        pushUnpushButtons("western", ["normal", "noir", "scifi"]);
+    } else if (id == "noir") {
+        pushUnpushButtons("noir", ["normal", "western", "scifi"]);
+    } else if (id == "scifi") {
+        pushUnpushButtons("scifi", ["normal", "western", "noir"]);
+    }
+}
+
+
+function setVideo(e) {
+    var id = e.target.getAttribute("id");
+    if (id == "video1") {
+        pushUnpushButtons("video1", ["video2"]);
+    } else if (id == "video2") {
+        pushUnpushButtons("video2", ["video1"]);
+    }
+}
+
+function pushUnpushButtons(idToPush, idArrayToUnpush) {
+    if (idToPush != "") {
+        var anchor = document.getElementById(idToPush);
+        var theClass = anchor.getAttribute("class");
+        if (!theClass.indexOf("selected") >= 0) {
+            theClass = theClass + " selected";
+            anchor.setAttribute("class", theClass);
+            var newImage = "url(images/" + idToPush + "pressed.png)";
+            anchor.style.backgroundImage = newImage;
+        }
+    }
+    for (var i = 0; i < idArrayToUnpush.length; i++) {
+        anchor = document.getElementById(idArrayToUnpush[i]);
+        theClass = anchor.getAttribute("class");
+        if (theClass.indexOf("selected") >= 0) {
+            theClass = theClass.replace("selected", "");
+            anchor.setAttribute("class", theClass);
+            anchor.style.backgroundImage = "";
+        }
+    }
+}
+
+function isButtonPushed(id) {
+    var anchor = document.getElementById(id);
+    var theClass = anchor.getAttribute("class");
+    return (theClass.indexOf("selected") >= 0);
+}
+
+function getFormatExtension() {
+    var video = document.getElementById("video");
+    
+    if(video.canPlayType("video/mp4") != "") {
+        return ".mp4";
+    } else if(video.canPlayType("video/webm") != "") {
+        return ".webm";
+    } else if(video.canPlayType("video/ogg") != "") {
+        return ".ogv";
+    }
+}
+
