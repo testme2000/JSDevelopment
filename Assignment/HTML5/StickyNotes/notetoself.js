f1882fa2e03797e61d0b1a3df8fff9c3d3a6f0a1
Nitin Dixit <nitin.dixit@ncr.com>
2018-07-06 12:49:59 -0500

    Added color to stickies

--- Assignment/HTML5/StickyNotes/notetoself.js
+++ Assignment/HTML5/StickyNotes/notetoself.js
@@ -3,63 +3,112 @@
 window.onload = init;
 
 function init() {
-    var button = document.getElementById("add_button");
-    button.onclick = createSticky;
+	var button = document.getElementById("add_button");
+	button.onclick = createSticky;
+/*
+	var clearButton = document.getElementById("clear_button");
+	clearButton.onclick = clearStickyNotes;
+*/
 
-    var stickiesArray = getStickiesArray();
+	var stickiesArray = localStorage["stickiesArray"];
+	if (!stickiesArray) {
+		stickiesArray = [];
+		localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray));
+	}
+	else {
+		stickiesArray = JSON.parse(stickiesArray);
+	}
 
-    for(var count = 0;count < stickiesArray.length;count++) {
-        var key = stickiesArray[count];
-        var value = localStorage[key];
-        addStickyToDOM(value);
-    }
-}
-
-function addStickyToDOM(value) {
-    var stickies = document.getElementById("stickies");
-    var sticky = document.createElement("li");
-    var span = document.createElement("span");
-    
-    span.setAttribute("class","sticky");
-    span.innerHTML = value;
-    sticky.appendChild(span);
-    stickies.appendChild(sticky);
-}
-
-function createSticky() {
-    var stickiesArray = getStickiesArray();
-    var currentDate = new Date();
-    var key = "sticky_" + currentDate.getTime();
-    var value = document.getElementById("note_text").value;
-    localStorage.setItem(key, value);
-    stickiesArray.push(key);
-    localStorage.setItem("stickiesArray",JSON.stringify(stickiesArray));
-    addStickyToDOM(value);
+	// replace with
+	// var stickiesAray = getStickiesArray();
+	
+	for (var i = 0; i < stickiesArray.length; i++) {
+		var key = stickiesArray[i];
+		var value = JSON.parse(localStorage[key]);
+		addStickyToDOM(key, value);
+	}	
 }
 
 function getStickiesArray() {
-    var stickiesArray = localStorage.getItem("stickiesArray");
-    if(!stickiesArray) {
-        stickiesArray = [];
-        localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray));
-    }
-    else {
-        stickiesArray = JSON.parse(stickiesArray);
-    }
+	var stickiesArray = localStorage.getItem("stickiesArray");
+	if (!stickiesArray) {
+		stickiesArray = [];
+		localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray));
+	} else {
+		stickiesArray = JSON.parse(stickiesArray);
+	}
+	return stickiesArray;
+}
+
+function createSticky() {
+	var stickiesArray = getStickiesArray();
+	var currentDate = new Date();
+    var colorSelectObj = document.getElementById("note_color");
+    var index = colorSelectObj.selectedIndex;
+    var color = colorSelectObj[index].value;
+	var key = "sticky_" + currentDate.getTime();
+	var value = document.getElementById("note_text").value;
+    var stickyObj = {
+        "value": value,
+        "color": color
+    };
+	localStorage.setItem(key, JSON.stringify(stickyObj));
+	stickiesArray.push(key);
+	localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray));
+	
+	addStickyToDOM(key, stickyObj);
+}
+
+function deleteSticky(e) {
+	var key = e.target.id;
+	if (e.target.tagName.toLowerCase() == "span") {
+		key = e.target.parentNode.id;
+	}
+	localStorage.removeItem(key);
+	var stickiesArray = getStickiesArray();
+	if (stickiesArray) {
+		for (var i = 0; i < stickiesArray.length; i++) {
+			if (key == stickiesArray[i]) {
+				stickiesArray.splice(i,1);
+			}
+		}
+		localStorage.setItem("stickiesArray", JSON.stringify(stickiesArray));
+		removeStickyFromDOM(key);
+	}
+}
+
+
+function addStickyToDOM(key, stickyObj) {
+	var stickies = document.getElementById("stickies");
+	var sticky = document.createElement("li");
+	sticky.setAttribute("id", key);
     
-    return stickiesArray;
+    sticky.style.backgroundColor = stickyObj.color;
+    
+	var span = document.createElement("span");
+	span.setAttribute("class", "sticky");
+	span.innerHTML = stickyObj.value;
+	sticky.appendChild(span);
+	stickies.appendChild(sticky);
+
+	sticky.onclick = deleteSticky;
+}
+
+function removeStickyFromDOM(key) {
+	var sticky = document.getElementById(key);
+	sticky.parentNode.removeChild(sticky);
 }
 
 
-function deleteSticky(key) {
-    localStorage.removeItem(key);
-    var stickiesArray = getStickiesArray();
-    if(stickiesArray) {
-        for(var count = 0;count < stickiesArray.length;count++) {
-            if(key == stickiesArray[count]) {
-                stickiesArray.splice(count,1);
-            }
-        }
-        localStorage.setItem("stickiesArray",stickiesArray);
-    }
+function clearStickyNotes() {
+	localStorage.clear();
+	var stickyList = document.getElementById("stickies");
+	var stickies = stickyList.childNodes;
+	for (var i = stickies.length-1; i >= 0; i--) {
+		stickyList.removeChild(stickies[i]);
+	}
+
+	// reset notes array
+	var stickiesArray = getStickiesArray();
 }
+
