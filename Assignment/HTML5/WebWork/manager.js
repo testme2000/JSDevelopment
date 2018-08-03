23e258ae4beb0483dd8917ef6a47d6f751ab2d2f
Nitin Dixit <nitin.dixit@ncr.com>
2018-08-03 14:02:17 -0500

    Vue.js - Directive example

--- Assignment/HTML5/WebWork/manager.js
+++ Assignment/HTML5/WebWork/manager.js
@@ -1,14 +1,21 @@
 
 
 window.onload = function()  {
-    var worker = new Worker("worker.js");
+    var numWorkers = 3;
+    var workers = [];
     
-    worker.postMessage("ping");
-    
-    worker.onmessage = function(event) {
-        var message = "Worker says " + event.data;
-        
-        document.getElementById("output").innerHTML = message;
+    for(var i = 0; i < numWorkers; i++) {
+        var worker = new Worker("worker.js");
+
+        worker.onmessage = function(event) {
+            alert(event.target + "says "+ event.data);
+        }
+        workers.push(worker);
+
+    }
+
+    for(var i = 0; i < numWorkers; i++) {
+        workers[i].postMessage("ping");
     }
 }
 
