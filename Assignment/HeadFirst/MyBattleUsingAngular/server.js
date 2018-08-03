b90b3146191e0e1f77c27ea3c4cf2929336a9dde
testme2000 <nitin.dixfree@gmail.com>
2018-08-02 20:26:53 -0500

    Created Express Server for AngularJS APP

--- Assignment/HeadFirst/MyBattleUsingAngular/server.js
+++ Assignment/HeadFirst/MyBattleUsingAngular/server.js
@@ -2,11 +2,13 @@
 
 var express = require('express');
 var app = express();
-app.use(express.static("MyBattleUsingAngular"));
+app.use(express.static(__dirname + "/"));
 
 app.get('/', function(req,res,next) {
+    console.log("TEst");
     res.redirect('/');
 });
 
-app.listen(8080,'localhost');
-console.log("Battleship Server started");
+app.listen(8080,function() {
+    console.log("Battleship Server started on Port 8080");
+});
