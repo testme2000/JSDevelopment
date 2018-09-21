b56723324002ef52154043b009bae3f1b386969f
Nitin Dixit <nitin.dixit@ncr.com>
2018-09-21 15:17:50 -0500

    Stock application updated

--- Assignment/Vue/StockDetails/main.js
+++ Assignment/Vue/StockDetails/main.js
@@ -1,9 +1,15 @@
 "use strict";
 // MOMNOAE88JPG3RGL API Key
-new Vue({
+var app = new Vue({
     el : '#stockapp',
     data : {
         stockname : ""
+    },
+
+    methods: {
+        GetStockDetails : function() {
+            alert("This is testing");
+        }
     }
 
 });
