267adee7276ed6297f61428d878d0ee86143c486
testme2000 <nitin.dixfree@gmail.com>
2018-09-23 14:44:46 -0500

    Stock Project updated with REST API and parsing the result

--- Assignment/Vue/StockDetails/main.js
+++ Assignment/Vue/StockDetails/main.js
@@ -1,14 +1,41 @@
 "use strict";
 // MOMNOAE88JPG3RGL API Key
+
+Vue.component('stock-details',{
+    props : ['stock-object'],
+    template : '<h4>Result : </h4>'
+})
+ 
+
 var app = new Vue({
     el : '#stockapp',
     data : {
-        stockname : ""
+        stockname : "",
+        resultArrived : false,
+        fetchStatus: false,
+        resultDetails: {
+            Symbol : '',
+            LastUpdated: ''
+        }
     },
 
     methods: {
         GetStockDetails : function() {
-            alert("This is testing");
+            var basicUrl = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=";
+            basicUrl += this.stockname;
+            basicUrl += "&apikey=MOMNOAE88JPG3RGL";
+            axios.get(basicUrl).then(result => {
+                console.log(result); 
+                var output = result.data['Meta Data'];
+                this.resultDetails.Symbol = output['2. Symbol'];
+                this.resultDetails.LastUpdated = output['3. Last Refreshed'];
+                this.resultArrived = true;
+                this.fetchStatus = true;
+             }, error => {
+                var error = "Information not found";
+                this.resultArrived = false;
+                this.fetchStatus = true;
+             });
         }
     }
 
--- Assignment/Vue/VueMastery/Main.js
+++ Assignment/Vue/VueMastery/Main.js
@@ -5,11 +5,11 @@ var product = "Socks";
 var app = new Vue({
     el: '#App',
     data : {
+        brand: 'MasterGap',
         product : 'Socks',
         description: 'A pair of warm, fuzzy socks',
-        image:"./vmSocks-green-onWhite.jpg",
+        selectedVariant : 0,
         ProductLink:'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-        onSale: true,
         showproductlink: true,
         Details: ["80% Cotton","20% Polyster","Gender-neutral"],
         Sizes: ["Small","Medium","Large","Ex-Large"],
@@ -17,28 +17,55 @@ var app = new Vue({
             {
                 variantId : 2234,
                 variantColor: "Green",
-                variantImage: "./vmSocks-green-onWhite.jpg"
+                variantImage: "./vmSocks-green-onWhite.jpg",
+                variantQuantity: 5,
+                variantOnSale: false
             },
             {
                 variantId : 2235,
                 variantColor: "Blue",
-                variantImage: "./vmSocks-blue-onWhite.jpg"
+                variantImage: "./vmSocks-blue-onWhite.jpg",
+                variantQuantity: 10,
+                variantOnSale: true
             }
         ],
-        cart: 0,
-        inStock: true
+        cart: 0
     },
     methods : {
         addToCart : function() {
             this.cart++;
         },
 
-        updateProduct : function(variantImage) {
-            this.image = variantImage;
+        updateProduct : function(index) {
+            this.selectedVariant = index;
+            console.log("selected variant " +  index);
         },
 
         removeFromCart : function() {
             this.cart--;
         }
+    },
+    computed : {
+        caption : function() {
+            return this.brand + " " + this.product;
+        },
+
+        image : function() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+
+        inStock : function() {
+            return this.variants[this.selectedVariant].variantQuantity;
+        },
+
+        Sale : function() {
+            if(this.variants[this.selectedVariant].variantOnSale) {
+                return "Hurry Big discount " + this.brand + " " + this.product;
+            }
+            else {
+                return "Sale " + this.brand + " " + this.product;
+            }
+        }
+        
     }
 });
