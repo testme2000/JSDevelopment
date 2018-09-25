bbc24985d2cc10ce5ffc1c139b6ab48ac02eca02
Nitin Dixit <nitin.dixit@ncr.com>
2018-09-25 16:52:12 -0500

    REST Api result processed and displayed in vue component

--- Assignment/Vue/StockDetails/main.js
+++ Assignment/Vue/StockDetails/main.js
@@ -2,8 +2,37 @@
 // MOMNOAE88JPG3RGL API Key
 
 Vue.component('stock-details',{
-    props : ['stock-object'],
-    template : '<h4>Result : </h4>'
+    props : ['stockObject'],
+    template : `
+        <div>
+        <h4>Result :</h4> 
+        <table>
+            <tr>
+                <td>Symbol:</td>
+                <td>{{stockObject.Symbol}}</td>
+            </tr>
+            <tr>
+                <td>Last Updated:</td>
+                <td>{{stockObject.LastUpdated}}</td>
+            </tr>
+            <tr>
+                <td>Open:</td>
+                <td>{{stockObject.Open}}</td>
+                <td>Close:</td>
+                <td>{{stockObject.Close}}</td>
+            </tr>
+            <tr>
+                <td>High:</td>
+                <td>{{stockObject.High}}</td>
+                <td>Low:</td>
+                <td>{{stockObject.Low}}</td>
+            </tr>
+            <tr>
+                <td>Dividend Amount</td>
+                <td>{{stockObject.DividendAmount}}
+            </tr>
+        </table>
+        </div>`
 })
  
 
@@ -15,8 +44,14 @@ var app = new Vue({
         fetchStatus: false,
         resultDetails: {
             Symbol : '',
-            LastUpdated: ''
-        }
+            LastUpdated: '',
+            Open : '',
+            Close : '',
+            High : '',
+            Low : '',
+            DividendAmount : 0.0
+        },
+        testme : "Check it out"
     },
 
     methods: {
@@ -25,12 +60,22 @@ var app = new Vue({
             basicUrl += this.stockname;
             basicUrl += "&apikey=MOMNOAE88JPG3RGL";
             axios.get(basicUrl).then(result => {
-                console.log(result); 
+                // Get basic stock information
                 var output = result.data['Meta Data'];
                 this.resultDetails.Symbol = output['2. Symbol'];
                 this.resultDetails.LastUpdated = output['3. Last Refreshed'];
+                // Stock volume information
+                var stocktrans = result.data['Time Series (Daily)'];
+                // Split the lastupdate datetime to get only date part 
+                let datedetails = this.resultDetails.LastUpdated.split(' ');
+                let datepart = datedetails[0];   
                 this.resultArrived = true;
                 this.fetchStatus = true;
+                this.resultDetails.Open = stocktrans[datepart]['1. open'];
+                this.resultDetails.Close = stocktrans[datepart]['4. close'];
+                this.resultDetails.High = stocktrans[datepart]['2. high'];
+                this.resultDetails.Low = stocktrans[datepart]['3. low'];
+                this.resultDetails.DividendAmount = stocktrans[datepart]['7. dividend amount'];
              }, error => {
                 var error = "Information not found";
                 this.resultArrived = false;
