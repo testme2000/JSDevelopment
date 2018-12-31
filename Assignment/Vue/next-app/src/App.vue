23a9dc96ac9e7d508661d75669ccd22a344e0385
Nitin Dixit <nitin.dixit@ncr.com>
2018-12-31 13:27:55 -0600

    Playing with zero configuration for Vue

---
+++ Assignment/Vue/next-app/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>{{title}}</h1>
+</template>
+
+<script>
+    import Vue from 'vue'
+
+    export default {
+        name : 'app',
+        data() {
+            return { title : 'Hello Zero configuration Vue!'}
+        },
+    }
+
+</script>
