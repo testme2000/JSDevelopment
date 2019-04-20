7fd12da3ddc194d0f1b89c9caa313e94a43ff9da
testme2000 <nitin.dixfree@gmail.com>
2019-04-20 15:20:51 -0500

    Extroplanet apps created to review the javascript promises

---
+++ Assignment/Vue/extroplanet/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <SearchPlanet/>
+  </div>
+</template>
+
+<script>
+import SearchPlanet from './components/PlanetSearch.vue'
+
+export default {
+  name: 'app',
+  components: {
+    SearchPlanet
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Assignment/Vue/extroplanet/src/components/PlanetSearch.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1 v-show=status>Searching for planet like earth</h1>
+        <h2>{{searchresult}}</h2>
+    </div>    
+</template>
+
+<script>
+import { setTimeout } from 'timers';
+    export default {
+        name : 'ExtraPlanet',
+        data : function() {
+            return {
+                status : true,
+                searchresult : ''
+            }
+        },
+        mounted : function() {
+            setTimeout(function() {
+                this.startSearch();
+            }.bind(this),2000);
+        },
+        methods : {
+            startSearch : function() {
+                this.status = false;
+                this.searchresult = 'Trying to find something, please wait..................';
+            }
+        } 
+    }
+
+
+</script>
