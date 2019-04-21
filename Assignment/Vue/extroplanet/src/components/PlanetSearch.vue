42c33768e44ee0889bbc464d3701fb98046a867a
testme2000 <nitin.dixfree@gmail.com>
2019-04-21 13:42:24 -0500

    Trying to add HTTP Request call but getting error, it need either axios or vue-resource, lets try vue-resource this time

--- Assignment/Vue/extroplanet/src/components/PlanetSearch.vue
+++ Assignment/Vue/extroplanet/src/components/PlanetSearch.vue
@@ -7,6 +7,9 @@
 
 <script>
 import { setTimeout } from 'timers';
+import { resolve } from 'dns';
+import { Promise } from 'q';
+
     export default {
         name : 'ExtraPlanet',
         data : function() {
@@ -24,6 +27,38 @@ import { setTimeout } from 'timers';
             startSearch : function() {
                 this.status = false;
                 this.searchresult = 'Trying to find something, please wait..................';
+                /*
+                // Search the planet now
+                getplanet('http://udacity.github.io/exoplanet-explorer/site/app/data/earth-like-results.json')
+                .then(function(response) {
+                    this.searchresult = response.statusText;
+                })
+                .catch(function(reject) {
+                    this.searchresult = response.statusText;
+                });
+                */
+            },
+            getplanet : function(url) {
+                /*
+                return new Promise(function(resolve,reject) {
+                    var request = new XMLHttpRequest();
+
+                    request.open('GET',url);
+                    request.onload = function() {
+                        if(request.status === 200) {
+                            resolve(request.response);
+                        }
+                        else {
+                            reject(Error(request.statusText));
+                        }
+                    };
+                    request.onerror = function() {
+                        reject("Network Error");
+                    }
+                    request.send();
+                });
+                */
+               var tst = url;
             }
         } 
     }
